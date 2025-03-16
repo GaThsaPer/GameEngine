f1156ccca9c82c7d9ebcf88bc3d5d483507b4926
GaThsaPer <kacper1.grabowski@gmail.com>
2025-03-16 17:37:58 +0100

    First Labolatory

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,18 @@
 #include <raylib.h>
 
 int main(int argc, char *argv[]) {
+    //Window Size
+    constexpr Vector2 screenSize = {1280, 720};
+
+    //Initiation of the window
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow (screenSize.x, screenSize.y, "RayEngine");
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
     
     return 0;
 }
