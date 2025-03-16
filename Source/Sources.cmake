585b05e5291ed7cf46d0c55397ef01d35ed69347
GaThsaPer <kacper1.grabowski@gmail.com>
2025-03-16 17:17:05 +0100

    test_2 commit

---
+++ Source/Sources.cmake
@@ -0,0 +1,9 @@
+# Define sources
+target_sources(${PROJECT_NAME} PRIVATE
+        # Add project source files here
+        # Example:
+        # ${SOURCE_DIR}/Folder/Example.cpp
+        # ${SOURCE_DIR}/Folder/Example.h
+
+        # ---
+)
